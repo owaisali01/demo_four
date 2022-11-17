22aa0b1f58f728eb97f08693f566cd0edff7003c
owaisali01 <owais.ali9091@gmail.com>
2022-11-17 23:52:14 +0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoFour::Application.config.secret_key_base = '771ae31993206f3f8525ae0d8defadada15ea7669d7ad0d80a012fb2d017fb6942ff498d77f3eda7d986786c52336a89be390738c962d0fa28a1cad60ebdc368'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoFour::Application.config.session_store :cookie_store, key: '_demo_four_session'
